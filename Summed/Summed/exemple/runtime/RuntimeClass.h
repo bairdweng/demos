4f098ec946d69ab4abba1afdd99403b659959c30
Baird-weng <644672334@qq.com>
2019-12-20 19:09:16 +0800

    update

---
+++ Summed/Summed/exemple/runtime/RuntimeClass.h
@@ -0,0 +1,17 @@
+//
+//  RumtimeClass.h
+//  Summed
+//
+//  Created by Baird-weng on 2019/12/5.
+//  Copyright © 2019 bairdweng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RumtimeClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Summed/Summed/exemple/runtime/RuntimeClass.m
@@ -0,0 +1,13 @@
+//
+//  RumtimeClass.m
+//  Summed
+//
+//  Created by Baird-weng on 2019/12/5.
+//  Copyright © 2019 bairdweng. All rights reserved.
+//
+
+#import "RumtimeClass.h"
+
+@implementation RumtimeClass
+
+@end
